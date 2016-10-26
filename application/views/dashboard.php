4b2b3f85ce1d6c2ed91a3073284b554e5e98cee6
Aram <aram@rllinsure.com>
2016-10-26 23:24:11 +0400

    First commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,12 @@
+<?php $this->load->view('header'); ?>
+<div id="container">
+	<h1>Accurate</h1>
+
+	<div id="body">
+		<p>Here will be the table</p>
+
+		<a href="<?php echo site_url('products'); ?>"><button>Products</button></a>
+	</div>
+
+</div>
+<?php $this->load->view('footer'); ?>
---
+++ application/views/products.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('header'); ?>
+<div class="products">
+    <a href="<?php echo site_url('products/add_product'); ?>"><button>add product</button></a>
+</div>
+<?php $this->load->view('footer'); ?>
